c5073c7d66b6e7a6e459a5857619f3f69ed5a1d8
Ryan Raba <ryanraba@gmail.com>
2020-07-11 14:10:56 -0400

    fix notebook construction

--- scraper/_convert_html.py
+++ scraper/_convert_html.py
@@ -21,8 +21,10 @@ blank_meta = {"colab": {"provenance": [],"collapsed_sections": [], "toc_visible"
 # grab the list of all pages in casadocs
 with open('scraper/_sitemap.txt') as fid:
     urls = fid.read().splitlines()
-#urls = ['https://casa.nrao.edu/casadocs-devel/stable/imaging/synthesis-imaging/wide-band-imaging']
-
+#urls = ['https://casa.nrao.edu/casadocs-devel/stable/imaging/synthesis-imaging/wide-field-imaging-full-primary-beam']
+#urls = ['https://casa.nrao.edu/casadocs-devel/stable/imaging/synthesis-imaging']
+#urls = ['https://casa.nrao.edu/casadocs-devel/stable/global-task-list']
+#url = urls[0]
 # each page in casadocs should have already been downloaded by the scrapy spider to an html file
 # the local html directory structure should match the casadocs website structure
 # we will execute pandoc on each of these files to convert their format
@@ -40,7 +42,7 @@ for ii, url in enumerate(urls):
         dest = '/'.join(fpath) + '.ipynb'
         
         # use pandoc to convert html to markdown
-        os.system('pandoc %s -s -f html -t ipynb -o %s --atx-headers --extract-media=%s' % (source, dest, '/'.join(fpath[:-1])))
+        os.system('pandoc %s -s -f html -t ipynb -o %s --atx-headers --extract-media=%s --ascii' % (source, dest, '/'.join(fpath[:-1])))
         #os.system('pandoc %s -f html -t rst -o %s' % (source, dest+'.rst'))
         #os.system('pandoc %s -f rst -t ipynb -o %s' % (dest+'.rst', dest+'.ipynb'))
         
@@ -53,17 +55,17 @@ for ii, url in enumerate(urls):
         md = re.sub('</span>', '', md)
         
         # fix image links to work properly
-        md = re.sub('<img src="(.+?)" .+?/>', r'![image](\1)', md)
+        md = re.sub('<img src="(.+?)" .+?/>', r'![\1](\1)', md)
         md = re.sub('attachment:%s/' % '/'.join(fpath[:-1]), '', md)
         
         # split sections in to separate cells at appropriate level
-        splits = re.split('(#+([^#]|\n)+)', md)
-        splits = [split for split in splits if split.strip()]
-        for ii, split in enumerate(splits):
+        splits = re.split(r'(#+\s)', md)[1:]
+        #splits = [split for split in splits if split.strip()]
+        for ii in range(0, len(splits), 2):
             if ii == 0:
-                nb.cells[0] = nbformat.v4.new_markdown_cell(split)
+                nb.cells[0] = nbformat.v4.new_markdown_cell(splits[0]+splits[1])
             else:
-                nb.cells += [nbformat.v4.new_markdown_cell('#'+split)]
+                nb.cells += [nbformat.v4.new_markdown_cell('#'+splits[ii]+splits[ii+1])]
         
         #nb.cells[0]['source'] = md
         nbformat.write(nb, dest, nbformat.NO_CONVERT)
