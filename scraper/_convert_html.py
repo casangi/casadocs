8941306e30636f4487f2beaae4befc3ea2f935f1
Ryan Raba <ryanraba@gmail.com>
2020-07-10 20:12:37 -0400

    convert to ipynb output

--- scraper/_convert_html.py
+++ scraper/_convert_html.py
@@ -1,4 +1,6 @@
 import os
+import re
+import nbformat
 
 # toctree pages
 header = """
@@ -12,10 +14,14 @@ body = """
 
 """
 
+# notebook template
+blank_meta = {"colab": {"provenance": [],"collapsed_sections": [], "toc_visible": True, "include_colab_link": True},
+                        "kernelspec": {"name": "python3", "display_name": "Python 3"}}
 
 # grab the list of all pages in casadocs
 with open('scraper/_sitemap.txt') as fid:
     urls = fid.read().splitlines()
+#urls = ['https://casa.nrao.edu/casadocs-devel/stable/imaging/synthesis-imaging/wide-band-imaging']
 
 # each page in casadocs should have already been downloaded by the scrapy spider to an html file
 # the local html directory structure should match the casadocs website structure
@@ -31,29 +37,57 @@ for ii, url in enumerate(urls):
             subdir = '/'.join(fpath[:ii])
             if not os.path.isdir(subdir):
                 os.mkdir(subdir)
-        dest = '/'.join(fpath) + '.rst'
-        #os.system('pandoc %s -f html -t ipynb -o %s' % (source, dest))
-        os.system('pandoc %s -f html -t rst -o %s' % (source, dest))
+        dest = '/'.join(fpath) + '.ipynb'
         
-        # add each file to the toctree of the parent folder
+        # use pandoc to convert html to markdown
+        os.system('pandoc %s -s -f html -t ipynb -o %s --atx-headers --extract-media=%s' % (source, dest, '/'.join(fpath[:-1])))
+        #os.system('pandoc %s -f html -t rst -o %s' % (source, dest+'.rst'))
+        #os.system('pandoc %s -f rst -t ipynb -o %s' % (dest+'.rst', dest+'.ipynb'))
+        
+        # now we need to manually fix the notebook to conform to nbsphinx conversion limitations
+        nb = nbformat.read(dest, nbformat.NO_CONVERT)
+        md = nb.cells[0]['source']
+        
+        # get rid of extraneous span tags
+        md = re.sub('<span(.|\n)*?>','', md)
+        md = re.sub('</span>', '', md)
+        
+        # fix image links to work properly
+        md = re.sub('<img src="(.+?)" .+?/>', r'![image](\1)', md)
+        md = re.sub('attachment:%s/' % '/'.join(fpath[:-1]), '', md)
+        
+        # split sections in to separate cells at appropriate level
+        splits = re.split('(#+([^#]|\n)+)', md)
+        splits = [split for split in splits if split.strip()]
+        for ii, split in enumerate(splits):
+            if ii == 0:
+                nb.cells[0] = nbformat.v4.new_markdown_cell(split)
+            else:
+                nb.cells += [nbformat.v4.new_markdown_cell('#'+split)]
+        
+        #nb.cells[0]['source'] = md
+        nbformat.write(nb, dest, nbformat.NO_CONVERT)
+        
+        ## add each file to the toctree of the parent folder
         if len(fpath) > 2:
-            fname = '/'.join(fpath[:-1])+'.rst'
-            if not os.path.exists(fname):  # if the parent folder doesn't have its own page
-                with open(fname, 'w') as fid:
-                    fid.write(fpath[-2] + header + body + '   %s' % ('/'.join(fpath[-2:])))
-            else:   # see if it already has a toctree
-                with open(fname) as fid:
-                    lines = fid.read().splitlines()
-                if '.. toctree::' not in lines:
-                    with open(fname, 'a') as fid:
-                        fid.write('\n' + body + '   %s' % ('/'.join(fpath[-2:])))
-                else:  # just add this file to the existing toctree
-                    with open(fname, 'a') as fid:
-                        fid.write('\n   %s' % ('/'.join(fpath[-2:])))
-                
-
+            # we need to extract the proper title
+            with open(source, 'r') as fid:
+                lines = fid.read()
+            title = re.search('<h1 class="documentFirstHeading">(.+?)</h1>', lines).group(1)
+            
+            fname = '/'.join(fpath[:-1]) + '.ipynb'
+            nb = nbformat.read(fname, nbformat.NO_CONVERT)
+            
+            # the last cell should be the toctree, if not, create a new cell with one
+            if 'nbsphinx-toctree' not in nb.cells[-1]['metadata']:
+                toctree = nbformat.from_dict({'cell_type': 'markdown', 'metadata': {"nbsphinx-toctree": {"hidden": True}},'source': ""})
+                nb.cells += [toctree]
+            
+            nb.cells[-1]['source'] += "[%s](%s)\n" % (title, '/'.join(fpath[-2:]))
+            nbformat.write(nb, fname, nbformat.NO_CONVERT)
+        
 # make the root level file the index
-os.system("mv docs/_files.rst docs/index.rst")
+os.system("mv docs/_files.ipynb docs/index.ipynb")
 
 print('')
 print('done')
--- scraper/spiders/single_spider.py
+++ scraper/spiders/single_spider.py
@@ -8,20 +8,20 @@ rst_body = """.. toctree::
    :maxdepth: 2
    :glob:
 
-   
+
 """
 
-class CasadocsSpider(scrapy.Spider):
-    name = "full"
 
+class CasadocsSpider(scrapy.Spider):
+    name = "single"
+    
     def start_requests(self):
-        with open('scraper/_sitemap.txt') as fid:
-            urls = fid.read().splitlines()
+        urls = ['https://casa.nrao.edu/casadocs-devel/stable/imaging/synthesis-imaging/wide-band-imaging']
         
         for url in urls:
-            #yield scrapy.Request(url=url, callback=self.parse)
+            # yield scrapy.Request(url=url, callback=self.parse)
             yield SplashRequest(url, self.parse, endpoint='render.html', args={'wait': 2.0}, )
-
+    
     def parse(self, response):
         fpath = ['html'] + response.url.split("/")[4:]
         
@@ -29,7 +29,7 @@ class CasadocsSpider(scrapy.Spider):
         if 'redirect_urls' in response.request.meta.keys():
             fpath = ['html'] + response.request.meta['redirect_urls'][0].split("/")[4:]
         
-        for ii in range(1,len(fpath)):
+        for ii in range(1, len(fpath)):
             if not os.path.isdir('/'.join(fpath[:ii])):
                 os.mkdir('/'.join(fpath[:ii]))
         
@@ -42,27 +42,26 @@ class CasadocsSpider(scrapy.Spider):
         # now of course with task and tool redirects, the title may need to be adjusted again
         if fpath[-1].startswith('task_') or fpath[-1].startswith('tool_'):
             title = fpath[-1][5:]
-            
+        
         # main HTML of page
-        body = ''.join(response.css("#content").getall())  # .css("#parent-fieldname-text").getall())
+        body = ''.join(response.css("#content").getall())  #.css("#parent-fieldname-text").getall())
+        
+        # stick a proper heading back on top
+        #body = ("<!DOCTYPE html>\n <head><title>%s</title></head>\n" % title) + body
         
         # the main heading of the page is used later to set the new navigation and page name
         # sometimes the plone heading differs from the plone navigation tree, and the navigation tree is a better name
         # so here we overwrite the heading with whatever the navigation tree is calling this page
         body = re.sub('(<h1 class="documentFirstHeading">).*(</h1>)', r'\1%s\2' % title, body)
         
-        # deal with relative links to images
-        body = re.sub('\.\./\.\./\.\./\.\./\.\./docs/', 'https://casa.nrao.edu/docs/', body)
-        body = re.sub('\.\./\.\./\.\./\.\./docs/', 'https://casa.nrao.edu/docs/', body)
-        
         # plone folders/pages that list contents need to be removed, they will be replaced by toctrees and their links are bad anyway
         entries = response.css('div.entries').getall()
         if len(entries) > 0:
             body = body.replace(''.join(entries), '')
-            
+        
         # for some reason, mathjax has duplicative information that needs to be removed
-        #entries = response.css("span[class*=mjx]").getall()
-        #entries += response.css("span[class*=MJXp]").getall()
+        # entries = response.css("span[class*=mjx]").getall()
+        # entries += response.css("span[class*=MJXp]").getall()
         entries = response.css("span.mjx-chtml").getall()
         for entry in entries:
             body = body.replace(entry, '')
