93e70bd494a48bece00f0a8b91a99903797e8452
Ryan Raba <ryanraba@gmail.com>
2020-09-08 19:06:13 -0400

    remove numpy dependency

--- scripts/parse_tasks.py
+++ scripts/parse_tasks.py
@@ -2,11 +2,10 @@ import xml.etree.ElementTree as ET
 import requests
 import re
 import os
-import numpy as np
 
 # grab the index of all the task xml pages
 xmlstring = requests.get("https://casa.nrao.edu/PloneResource/stable/taskXml/").text
-tasknames = np.unique(re.findall("\w+.xml", xmlstring))
+tasknames = list(set(re.findall("\w+.xml", xmlstring)))
 
 # loop through each task xml webpage and parse the xml to python dictionaries
 tasklist = []
