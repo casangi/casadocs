fb09923dc568258256ebc338b8c68386616d0cd5
Andrew McNichols <amcnicho@nrao.edu>
2021-07-12 15:30:56 -0400

    Update casademo.py
    Reference smaller MS on disk
    Add explicit 'number' method definition
    Change test names back to match CAS-13490

--- benchmarks/casademo.py
+++ benchmarks/casademo.py
@@ -6,7 +6,7 @@ class flagdata_suite:
     """
     An example benchmark that adapts CAS-13490 to asv
     """
-    dataroot = ctsys.resolve('/.lustre/naasc/sciops/comm/scastro/casa/Tests/CAS-13490/performance/flagdata_runtime/')
+    dataroot = ctsys.resolve('/.lustre/naasc/sciops/comm/scastro/casa/Tests/CAS-13490/uid___A002_Xe1f219_X6d0b_data_autocorr_WRAY_scan7.ms')
     input_ms = 'uid___A002_Xe1f219_X6d0b_data_autocorr.ms'
     flags_cmd = 'uid___A002_Xe1f219_X6d0b.flagcmds.txt'
 
@@ -19,6 +19,9 @@ class flagdata_suite:
     def setup(self):
         # run for each repeated test
 
+        # iterations per sample
+        number = 5
+
         # Delete the MS if already exists
         if os.path.exists(self.input_ms):
             shutil.rmtree(self.input_ms)
@@ -30,18 +33,22 @@ class flagdata_suite:
         # assign our test dataset
         self.datapath = os.path.join(self.dataroot, self.input_ms)
 
-    def time_flagdata_hifa_importdata(self):
+    def time_flagdata_summary(self):
+        # hifa_importdata
         summary_dict = flagdata(vis=self.datapath, flagbackup=False, mode='summary')
 
-    def time_flagdata_hifa_flagdata(self):
+    def time_flagdata_list(self):
+        # hifa_flagdata
         flagdata(vis=self.datapath, mode='list', inpfile=self.flags_cmd, tbuff=[0.048, 0.0], 
                  action='apply', flagbackup=False, savepars=False)
 
-    def time_flagdata_hifa_rawflagchans(self):
+    def time_flagdata_list_summary(self):
+        # hifa_rawflagchans
         summary_dict = flagdata(vis=input_ms, mode='list', inpfile=["mode='summary' name='before'"], 
                                 reason='any', action='apply', flagbackup=False, savepars=False)
 
-    def time_flagdata_hifa_bandpassflag(self):
+    def time_flagdata_bandpassflag(self):
+        # hifa_bandpassflag
         flagdata(vis=self.datapath, mode='list',
                  inpfile=["intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='16' antenna='CM05' \
                  timerange='20:09:50~20:09:52' field='J1924-2914' reason='bad antenna timestamp'", 
