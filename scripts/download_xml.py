7d7f41e5d15eb41c2dc1b5daee13c5220979a9ad
ryanraba <ryanraba@gmail.com>
2021-10-12 11:15:25 -0400

    fix branch parsing of stable tag

--- scripts/download_xml.py
+++ scripts/download_xml.py
@@ -9,9 +9,21 @@ os.system('mkdir ../casasource')
 os.system('git branch > branch_name.txt')
 with open('branch_name.txt', 'r') as fid:
     branch_name = [ll for ll in fid.readlines() if ll.startswith('*')][0].strip().split('/')[-1].split(' ')[-1].replace(')','')
-    
+
+# if this is a release branch, we need to look in the release folder
+if len(re.findall('v\d\.', branch_name)) > 0:
+    branch_name = 'release/' + branch_name[1:]
+
 # see if this branch_name exists in the code repo
 xmlstring = requests.get("https://open-bitbucket.nrao.edu/rest/api/1.0/projects/CASA/repos/casa6/browse/casatasks/xml?at=refs/heads/%s"%branch_name).text
+tasknames = list(set(re.findall("\w+.xml", xmlstring)))
+if len(tasknames) == 0:
+    # this could be a tag (like a stable build) instead of a real branch, try to resolve the hash and see
+    os.system('git name-rev %s > branch_tag.txt' % branch_name)
+    with open('branch_tag.txt', 'r') as fid:
+        branch_name = 'release/' + fid.readlines()[0].strip().split('/')[-1].split('-')[0][1:]
+
+xmlstring = requests.get("https://open-bitbucket.nrao.edu/rest/api/1.0/projects/CASA/repos/casa6/browse/casatasks/xml?at=refs/heads/%s" % branch_name).text
 tasknames = list(set(re.findall("\w+.xml", xmlstring)))
 if len(tasknames) == 0:
     print('Cant find corresponding code repository, defaulting to master')
