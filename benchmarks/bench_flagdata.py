81b2fc72038f468482e3d77b59b8ec1fa770eadd
Sandra M Castro <scastro@eso.org>
2022-06-21 20:46:16 +0200

    Update bench_flagdata with new test cases from casa6 test_task_flagdata and cleaned up the script to conform with asv.

--- benchmarks/bench_flagdata.py
+++ benchmarks/bench_flagdata.py
@@ -1,98 +1,100 @@
 import os, shutil
+import numpy
 from casatools import ctsys
 from casatasks import flagdata
 
+# Helper functions for tests
+def create_input(str_text, filename):
+    """Save the string in a text file"""
+    inp = filename
+    cmd = str_text
+    # remove file first
+    if os.path.exists(inp):
+        os.system('rm -f ' + inp)
+    # save to a file
+    with open(inp, 'w') as f:
+        f.write(cmd)
+    f.close()
+    return
 
-class flagdata_suite:
-    """
-    An example benchmark that adapts CAS-13490 to asv
-    """
+# ASV attributes
+timeout = 10000
+number = 2
 
-    dataroot = ctsys.resolve(
-        os.path.join(
-            os.environ.get("ASV_CONF_DIR"), "casatestdata/performance/flagdata_runtime/"
-        )
-    )
-    input_ms = "uid___A002_Xe1f219_X6d0b_data_autocorr_WRAY_scan7.ms/"
-    flags_cmd = "uid___A002_Xe1f219_X6d0b.flagcmds.txt"
+class BaseFlagSetup():
+    # Test datasets; root directory is read from config.py
+    datapath = ctsys.resolve("unittest/taskname/")
 
-    timeout = 10000
+    def setUp_data4tfcrop(self):
+        """EVLA MS, 4 ants, scan=30,31 spw=0~15, 64 chans, RR,RL,LR,LL"""
+        self.vis = "Four_ants_3C286.ms"
+        if os.path.exists(self.vis):
+            os.system('rm -rf ' + self.vis + '*')
+        shutil.copytree(os.path.join(self.datapath,self.vis),self.vis)
 
-    def setup_cache(self):
-        # only run once for repeated tests
+    def setUp_ngc5921(self):
+        """VLA data set, scan=1~7, spw=0 63 chans, RR,LL"""
         pass
 
+
+class TaskModeSomething(BaseFlagSetup):
+    """
+    Benchmark runtime of taskname mode something
+    """
     def setup(self):
-        # run for each repeated test
+        self.setUp_data4tfcrop()
 
-        # iterations per sample
-        self.number = 2
+    def time_taskmode_someinfo(self):
+        """taskname: mode taskmode, .... (original test case name for reference)"""
+        taskname(vis=self.vis, ......)
 
-        # assign our test dataset
-        self.datapath = os.path.join(self.dataroot, self.input_ms)
+class TaskModeOther(BaseFlagSetup):
+    """
+    Benchmark runtime of taskname mode other
+    """
+    def setup(self):
+        self.setUp_data4tfcrop()
 
-        ## fresh copy of the test MS to the tmp directory where tests are run ?
-        # if not os.path.exists(self.input_ms):
-        #    shutil.copytree(os.path.join(self.dataroot, self.input_ms),
-        #                    os.path.join(os.getcwd(), self.input_ms))
+    def time_taskmodeother_something(self):
+        """taskname: ..... (original test case name)"""
+         taskname(vis=self.vis, ......)
 
-        # Copy the flagcmd text file into temporary test directory
-        if not os.path.exists(self.flags_cmd):
-            shutil.copyfile(
-                os.path.join(self.dataroot, self.flags_cmd),
-                os.path.join(os.getcwd(), self.flags_cmd),
-            )
+class TaskModeUsingSetupCache(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata using different selections
+    """
+    def setup_cache(self):
+        # Create antennafile in disk
+        myinput = 'name=VLA01\n' + \
+                  'diameter=25.0\n' + \
+                  'position=[-1601144.96146691, -5041998.01971858, 3554864.76811967]\n' + \
+                  'name=VLA02\n' + \
+                  'diameter=25.0\n' + \
+                  'position=[-1601105.7664601889, -5042022.3917835914, 3554847.245159178]\n' + \
+                  'name=VLA09\n' + \
+                  'diameter=25.0\n' + \
+                  'position=[-1601197.2182404203, -5041974.3604805721, 3554875.1995636248]\n' + \
+                  'name=VLA10\n' + \
+                  'diameter=25.0\n' + \
+                  'position=[-1601227.3367843349,-5041975.7011900628,3554859.1642644769]\n'
 
-    def time_flagdata_summary(self):
-        """hifa_importdata"""
-        summary_dict = flagdata(vis=self.datapath, flagbackup=False, mode="summary")
+        self.antfile = 'antfile.txt'
+        create_input(myinput, self.antfile)
 
-    def time_flagdata_list(self):
-        """hifa_flagdata"""
-        flagdata(
-            vis=self.datapath,
-            mode="list",
-            inpfile=self.flags_cmd,
-            tbuff=[0.048, 0.0],
-            action="apply",
-            flagbackup=False,
-            savepars=False,
-        )
+        # Create flagcmd file
+        myinput = "mode='shadow' tolerance=0.0 addantenna='antfile.txt'"
+        self.inpfile = 'listcmd.txt'
+        create_input(myinput, self.inpfile)
 
-    def time_flagdata_list_summary(self):
-        """hifa_rawflagchans"""
-        summary_dict = flagdata(
-            vis=self.datapath,
-            mode="list",
-            inpfile=["mode='summary' name='before'"],
-            reason="any",
-            action="apply",
-            flagbackup=False,
-            savepars=False,
-        )
-
-    def time_flagdata_bandpassflag(self):
-        """hifa_bandpassflag"""
-        flagdata(
-            vis=self.datapath,
-            mode="list",
-            inpfile=[
-                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='16' antenna='CM05' \
-                 timerange='20:09:50~20:09:52' field='J1924-2914' reason='bad antenna timestamp'",
-                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='20' antenna='CM05' \
-                          timerange='20:09:20~20:09:22' field='J1924-2914' reason='bad antenna timestamp'",
-                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='20' antenna='CM05' \
-                          timerange='20:10:21~20:10:22' field='J1924-2914' reason='bad antenna timestamp'",
-                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='22' antenna='CM05' \
-                          timerange='20:09:30~20:09:32' field='J1924-2914' reason='bad antenna timestamp'",
-            ],
-            reason="any",
-            action="apply",
-            flagbackup=False,
-            savepars=False,
-        )
+    def setup(self):
+        self.setUp_data4tfcrop()
 
     def teardown(self):
-        # remove the data products generated by the task
-        os.remove(self.flags_cmd)
-        shutil.rmtree(self.input_ms, ignore_errors=True)
+        if os.path.exists(self.inpfile):
+            os.system("rm -rf listcmd*.txt antfile*.txt withdict.txt")
+
+    def time_someothermode_info(self):
+        """taskname: ....; (original test case)"""
+        taskname(vis=self.vis, mode='list', inpfile=self.inpfile, savepars=True, outfile='withdict.txt',
+                 flagbackup=False)
+
