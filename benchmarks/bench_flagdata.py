a711554f064786903e86d9f0bbab54f4eb2ab490
Sandra M Castro <scastro@eso.org>
2022-06-21 21:15:12 +0200

    Fixed new bench_flagdata.py test scripts.

--- benchmarks/bench_flagdata.py
+++ benchmarks/bench_flagdata.py
@@ -23,7 +23,7 @@ number = 2
 
 class BaseFlagSetup():
     # Test datasets; root directory is read from config.py
-    datapath = ctsys.resolve("unittest/flagdata/")
+    datapath = ctsys.resolve("unittest/testdata/")
 
     def setUp_data4tfcrop(self):
         """EVLA MS, 4 ants, scan=30,31 spw=0~15, 64 chans, RR,RL,LR,LL"""
@@ -34,32 +34,233 @@ class BaseFlagSetup():
 
     def setUp_ngc5921(self):
         """VLA data set, scan=1~7, spw=0 63 chans, RR,LL"""
-        pass
+        self.vis = "ngc5921.ms"
+        if os.path.exists(self.vis):
+            os.system('rm -rf ' + self.vis + '*')
+        shutil.copytree(os.path.join(self.datapath,self.vis),self.vis)
 
+    def setUp_WRay_perf(self):
+        self.vis = "uid___A002_Xe1f219_X6d0b_data_autocorr_WRAY_scan7.ms/"
+        if os.path.exists(self.vis):
+            os.system('rm -rf ' + self.vis + '*')
+        shutil.copytree(os.path.join(self.datapath,self.vis),self.vis)
 
-class TaskModeSomething(BaseFlagSetup):
+    def setUp_alma_ms(self):
+        """ALMA MS, scan=1,8,10 spw=0~3 4,128,128,1 chans, I,XX,YY"""
+        self.vis = "uid___A002_X30a93d_X43e_small.ms"
+        if os.path.exists(self.vis):
+            os.system('rm -rf ' + self.vis + '*')
+        shutil.copytree(os.path.join(self.datapath, self.vis), self.vis)
+
+    def setUp_tsys(self):
+        self.vis = "X7ef.tsys"
+        if os.path.exists(self.vis):
+            os.system('rm -rf ' + self.vis + '*')
+        shutil.copytree(os.path.join(self.datapath, self.vis), self.vis)
+
+    def setUp_bpass(self):
+        self.vis = "cal.fewscans.bpass"
+        if os.path.exists(self.vis):
+            os.system('rm -rf ' + self.vis + '*')
+        shutil.copytree(os.path.join(self.datapath, self.vis), self.vis)
+
+    def setUp_shadowdata(self):
+        """ALMA ACA observation with one field in APP ref frame"""
+        self.vis = "shadowAPP.ms"
+        if os.path.exists(self.vis):
+            os.system('rm -rf ' + self.vis + '*')
+        shutil.copytree(os.path.join(self.datapath, self.vis), self.vis)
+
+class AntintMode(BaseFlagSetup):
     """
-    Benchmark runtime of taskname mode something
+    Benchmark runtime of flagdata in antint mode
     """
     def setup(self):
         self.setUp_data4tfcrop()
 
-    def time_taskmode_someinfo(self):
-        """taskname: mode taskmode, .... (original test case name for reference)"""
-        taskname(vis=self.vis, ......)
+    def time_antint_low_threshold_spw(self):
+        """flagdata: mode antint, with low threshold; (original test_antint_spw3_low_threshold)"""
+        flagdata(vis=self.vis, mode='antint', spw='3', antint_ref_antenna='ea01', minchanfrac=-.1)
+        res = flagdata(vis=self.vis, mode='summary', spw='3')
 
-class TaskModeOther(BaseFlagSetup):
+class CalibrationBandpass(BaseFlagSetup):
     """
-    Benchmark runtime of taskname mode other
+    Benchmark runtime of flagdata on bandpass calibration tables
+    """
+    def setup(self):
+        self.setUp_bpass()
+
+    def time_bandpass_clip_timerange(self):
+        """Flagdata: clip a timerange from field 3C286_A; (original test_cal_time1)"""
+         flagdata(vis=self.vis, mode='clip', timerange='<14:12:52',clipzeros=True,
+                 clipminmax=[0.,0.35], datacolumn='CPARAM',flagbackup=False)
+
+class CalibrationTsys(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata on Tsys calibration tables
+    """
+    def setup(self):
+        self.setUp_tsys()
+
+    def time_tsys_clip_fparam(self):
+        """Flagdata: flag default data column FPARAM; (original test_default_fparam)"""
+        flagdata(vis=self.vis, mode='clip', clipminmax=[0,500], flagbackup=False,
+                 datacolumn='FPARAM')
+
+class ClipMode(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata clip mode
     """
     def setup(self):
         self.setUp_data4tfcrop()
 
-    def time_taskmodeother_something(self):
-        """taskname: ..... (original test case name)"""
-         taskname(vis=self.vis, ......)
+    def time_clip_corrected_data(self):
+        """flagdata: clip CORRECTED data column; (original test_datacol_corrected)"""
+        flagdata(vis=self.vis, flagbackup=False, mode='clip', datacolumn='CORRECTED',
+                 clipminmax=[0.,10.])
 
-class TaskModeUsingSetupCache(BaseFlagSetup):
+    def time_clip_with_timeavg(self):
+        """flagdata: clip with time averaging in spw 9; (original test_timeavg_spw9_2scans)"""
+        flagdata(vis=self.vis, flagbackup=False, mode='clip', datacolumn='DATA', spw='9',
+                 timeavg=True, timebin='2s', clipminmax=[0.0, 0.08])
+
+
+class ElevationMode(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata in elevation mode
+    """
+    def setup(self):
+        self.setUp_ngc5921()
+
+    def time_elevation_interval(self):
+        """Flagdata: flag an interval between upper and lower elevations; (original test_interval)"""
+        flagdata(vis = self.vis,mode = 'elevation',lowerlimit = 55,upperlimit = 60,
+                  savepars=False,flagbackup=False)
+
+class ListMode(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata list mode used in the pipeline
+    """
+    def setup_cache(self):
+        self.flags_cmd = "uid___A002_Xe1f219_X6d0b.flagcmds.txt"
+        if not os.path.exists(self.flags_cmd):
+            shutil.copyfile(os.path.join(self.datapath, self.flags_cmd))
+
+    def setup(self):
+        self.setUp_WRay_perf()
+
+    def time_flagdata_list(self):
+        """Flagdata list mode from pipeline hifa_flagdata"""
+        flagdata(
+            vis=self.vis,
+            mode="list",
+            inpfile=self.flags_cmd,
+            tbuff=[0.048, 0.0],
+            action="apply",
+            flagbackup=False,
+            savepars=False,
+        )
+
+    def time_list_bandpassflag(self):
+        """Flagdata list mode from pipeline hifa_bandpassflag"""
+        flagdata(
+            vis=self.vis,
+            mode="list",
+            inpfile=[
+                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='16' antenna='CM05' \
+                 timerange='20:09:50~20:09:52' field='J1924-2914' reason='bad antenna timestamp'",
+                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='20' antenna='CM05' \
+                          timerange='20:09:20~20:09:22' field='J1924-2914' reason='bad antenna timestamp'",
+                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='20' antenna='CM05' \
+                          timerange='20:10:21~20:10:22' field='J1924-2914' reason='bad antenna timestamp'",
+                "intent='CALIBRATE_BANDPASS#ON_SOURCE' spw='22' antenna='CM05' \
+                          timerange='20:09:30~20:09:32' field='J1924-2914' reason='bad antenna timestamp'",
+            ],
+            reason="any",
+            action="apply",
+            flagbackup=False,
+            savepars=False,
+        )
+
+    def time_list_summary(self):
+        """Flagdata list mode from pipeline hifa_rawflagchans"""
+        summary_dict = flagdata(
+            vis=self.vis,
+            mode="list",
+            inpfile=["mode='summary' name='before'"],
+            reason="any",
+            action="apply",
+            flagbackup=False,
+            savepars=False,
+        )
+
+class ListFileMode(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata list mode with input files
+    """
+    def setup_cache(self):
+        # Create 3 flagcmd files and save to disk
+        myinput = "scan='1'\n" \
+                  "scan='2'\n" \
+                  "# a comment line\n" \
+                  "scan='3'"
+        self.inpfile1 = 'listcmd1.txt'
+        create_input(myinput, self.inpfile1)
+
+        myinput = "scan='5'\n" \
+                  " \n" \
+                  "scan='6'\n" \
+                  "scan='7'"
+        self.inpfile2 = 'listcmd2.txt'
+        create_input(myinput, self.inpfile2)
+
+        myinput = "scan='4' mode='clip' clipminmax=[0,4]"
+        self.inpfile3 = 'listcmd3.txt'
+        create_input(myinput, self.inpfile3)
+
+    def setup(self):
+        self.setUp_ngc5921()
+
+    def teardown(self):
+        if os.path.exists(self.inpfile1):
+            os.system("rm -rf listcmd*.txt")
+
+    def time_list_from_3files(self):
+        """Flagdata list mode reading commands from three files; (original test_file_CAS4819)"""
+        flagdata(vis=self.vis, mode='list', inpfile=[self.inpfile1, self.inpfile2, self.inpfile3],
+                 flagbackup=False)
+
+class RflagMode(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata rflag mode
+    """
+    def setup(self):
+        self.setUp_data4tfcrop()
+        self.t1 = [numpy.int32(1), 10, numpy.float32(0.1)]
+        self.t2 = [1, numpy.int16(11), numpy.float64(0.07)]
+
+    def time_rflag_numpy_types(self):
+        """flagdata: rflag, thresholds using numpy types; (original test_rflag_partial_thresholds)"""
+        flagdata(vis=self.vis, mode='rflag', spw='9,10', timedev=[self.t1, self.t2], freqdev=0.5,
+                 flagbackup=False, extendflags=False)
+
+class Selections(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata using different selections
+    """
+    def setup(self):
+        self.setUp_alma_ms()
+
+    def time_sel_abs_wvr(self):
+        """flagdata: clip ABS_WVR; (original test_abs_wvr)"""
+        flagdata(vis=self.vis, mode='clip',clipminmax=[0,50], correlation='ABS_WVR', savepars=False,
+                 flagbackup=False)
+
+    def time_sel_autocorr_wvr(self):
+        """flagdata: CAS-5286, do not flag auto-correlations in WVR data; (original test_autocorr_wvr)"""
+        flagdata(vis=self.vis, autocorr=True, flagbackup=False)
+
+class ShadowMode(BaseFlagSetup):
     """
     Benchmark runtime of flagdata using different selections
     """
@@ -87,14 +288,34 @@ class TaskModeUsingSetupCache(BaseFlagSetup):
         create_input(myinput, self.inpfile)
 
     def setup(self):
-        self.setUp_data4tfcrop()
+        self.setUp_shadowdata()
 
     def teardown(self):
         if os.path.exists(self.inpfile):
             os.system("rm -rf listcmd*.txt antfile*.txt withdict.txt")
 
-    def time_someothermode_info(self):
-        """taskname: ....; (original test case)"""
-        taskname(vis=self.vis, mode='list', inpfile=self.inpfile, savepars=True, outfile='withdict.txt',
+    def time_shadow_addantenna(self):
+        """flagdata: use antenna file in list mode; (original test_addantenna)"""
+        flagdata(vis=self.vis, mode='list', inpfile=self.inpfile, savepars=True, outfile='withdict.txt',
                  flagbackup=False)
+
+    def time_shadow_absent_antennas(self):
+        """flagdata: shadow by antennas not present in MS; (original test_CAS2399)"""
+        flagdata(vis=self.vis, mode='shadow', tolerance=0.0, addantenna=self.inpfile1, flagbackup=False)
+
+class TfcropMode(BaseFlagSetup):
+    """
+    Benchmark runtime of flagdata tfcrop mode
+    """
+    def setup(self):
+        self.setUp_data4tfcrop()
+
+    def time_tfcrop_abs_rr(self):
+        """flagdata:: flag with tfcrop mode an ABS_RR correlation; (original test_tfcrop1)"""
+        flagdata(vis=self.vis, mode='tfcrop', correlation='ABS_RR',ntime=51.0,spw='9',
+                 savepars=False, extendflags=False)
+
+    def time_tfcrop_extendflags(self):
+        """flagdata: flag with tfcrop mode and extend flags automatically"""
+        flagdata(vis=self.vis, mode='tfcrop', spw='0', extendflags=True, flagbackup=False)
 
