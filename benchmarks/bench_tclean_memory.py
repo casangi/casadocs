670e273e83684437e9847b865b87fa1a7f6b1d87
smcastro <smcastro@users.noreply.github.com>
2022-11-08 16:44:39 +0100

    Added new classes and test cases for memory benchmarks of  tclean.

--- benchmarks/bench_tclean_memory.py
+++ benchmarks/bench_tclean_memory.py
@@ -3,6 +3,8 @@ import re, os, shutil
 from casatools import ctsys
 from casatasks import casalog
 from casatasks import tclean
+from casatestutils.imagerhelpers import TestHelpers
+th = TestHelpers()
 
 # ASV iteration control (https://asv.readthedocs.io/en/stable/benchmarks.html#benchmark-attributes)
 number = 1            # i.e., always run the setup and teardown methods
@@ -11,7 +13,118 @@ rounds = 3            # amount of instances a "repeat block" is run to collect s
 min_run_count = 3     # enforce the min_repeat * rounds setting is met
 timeout = 3600        # conservative 1hr hard cap for duration of a single test execution
 
-class tclean_memory_suite:
+# Test datasets; root directory is read from config.py
+datapath = ctsys.resolve("unittest/tclean/")
+
+class BaseTcleanSetup():
+    epsilon = 0.05
+    cfcache = 'cfcach'
+    msfile = ""
+    img = "tst"
+    imsize = 100
+    # To use subdir in the output image names in some tests (CAS-10937)
+    img_subdir = 'refimager_tst_subdir'
+    parallel = False
+    nnode = 0
+
+    # Copy and cleanup datasets
+    def prepData(self, msname=""):
+        if msname != "":
+            self.msfile = msname
+        if (os.path.exists(self.msfile)):
+            os.system('rm -rf ' + self.msfile)
+        shutil.copytree(os.path.join(datapath, self.msfile), self.msfile)
+
+class MemoryMultiField(BaseTcleanSetup):
+    """Peak memory benchmarking tests for tclean on multiple fields"""
+    def setup(self):
+        self.prepData("refim_twopoints_twochan.ms")
+
+    def teardown(self):
+        os.system('rm -rf ' + self.img_subdir)
+        os.system('rm -rf ' + self.img + '*')
+        if (os.path.exists(self.msfile)):
+            os.system('rm -rf ' + self.msfile)
+
+    def peakmem_multifield_mfs_hogbom(self):
+        """tclean : Two fields, both mfs - test_multifield_both_mfs"""
+        th.write_file(self.img + '.out.txt',
+                           'imagename=' + self.img + '1\nnchan=1\nimsize=[80,80]\ncell=[8.0arcsec,8.0arcsec]\nphasecenter=J2000 19:58:40.895 +40.55.58.543\nusemask=user\nmask=circle[[40pix,40pix],10pix]')
+
+        ret = tclean(vis=self.msfile, imagename=self.img, imsize=100, cell='8.0arcsec',
+                     phasecenter="J2000 19:59:28.500 +40.44.01.50", outlierfile=self.img + '.out.txt', niter=10,
+                     deconvolver='hogbom', interactive=0, parallel=False)
+
+    def peakmem_multifield_mfs_mtmfs(self):
+        """ tclean: Two fields, both mt-mfs - test_multifield_both_mtmfs"""
+        th.write_file(self.img + '.out.txt',
+                           'imagename=' + self.img + '1\n\nimsize=[80,80]\ncell=[8.0arcsec,8.0arcsec]\nphasecenter=J2000 19:58:40.895 +40.55.58.543\nusemask=user\nmask=circle[[40pix,40pix],10pix]')
+
+        ret = tclean(vis=self.msfile, imagename=self.img, imsize=100, cell='8.0arcsec',
+                     phasecenter="J2000 19:59:28.500 +40.44.01.50", outlierfile=self.img + '.out.txt', niter=10,
+                     deconvolver='mtmfs', interactive=0, parallel=False)
+
+    def peakmem_multifield_cube_hogbom(self):
+        """tclean: Two fields, both cube - test_multifield_both_cube"""
+        th.write_file(self.img + '.out.txt',
+                           'imagename=' + self.img + '1\nimsize=[80,80]\ncell=[8.0arcsec,8.0arcsec]\nphasecenter=J2000 19:58:40.895 +40.55.58.543\nimagename=' + self.img + '2\nimsize=[80,80]\ncell=[8.0arcsec,8.0arcsec]\nphasecenter=J2000 19:58:48.895 +40.55.58.543\n')
+
+        retpar = tclean(vis=self.msfile, imagename=self.img, imsize=100, cell='8.0arcsec',
+                        phasecenter="J2000 19:59:28.500 +40.44.01.50", outlierfile=self.img + '.out.txt', niter=10,
+                        deconvolver='hogbom', interactive=0, specmode='cube', nchan=2, interpolation='nearest',
+                        parallel=False)
+
+class MemoryWideField(BaseTcleanSetup):
+    """Peak memory benchmarking tests of tclean on widefield using mosaic gridder with cube mode"""
+    def setup(self):
+        self.prepData('refim_oneshiftpoint.mosaic.ms')
+
+    def peakmem_cube_mosaic_cbFalse_mwFalse_twofield(self):
+        """tclean: - cube mosaic using conjbeams=False - test_cube_mosaic_cbFalse_mwFalse_twofield"""
+        tclean(vis=self.msfile, imagename=self.img,niter=10,specmode='cube',spw='*',imsize=1024, phasecenter='J2000 19h59m28.5 +40d40m01.5',cell='10.0arcsec',gridder='mosaic',field='0,1', conjbeams=False, wbawp=True, psterm=False,pblimit=0.1,reffreq='1.5GHz',pbcor=True,mosweight=False,parallel=False)
+
+    def peakmem_cube_mosaic_cbFalse_mwTrue_twofield(self):
+        """tclean: - cube mosaic using conjbeams=False - test_cube_mosaic_cbFalse_mwTrue_twofield"""
+        tclean(vis=self.msfile, imagename=self.img,niter=10,specmode='cube',spw='*',imsize=1024, phasecenter='J2000 19h59m28.5 +40d40m01.5',cell='10.0arcsec',gridder='mosaic',field='0,1', conjbeams=False, wbawp=True, psterm=False,pblimit=0.1,reffreq='1.5GHz',pbcor=True,mosweight=True,parallel=False)
+
+    def peakmem_cube_mosaic_cbFalse_mwFalse_twofield_upTrue(self):
+        """tclean: Cube mosaic with conjbeams=F, mosaicweight=F, usepointing=T, of two fields -  test_cube_mosaic_cbFalse_mwFalse_twofield_upTrue"""
+        tclean(vis=self.msfile, imagename=self.img,niter=10,specmode='cube',spw='*',imsize=1024, phasecenter='J2000 19h59m28.5 +40d40m01.5',cell='10.0arcsec',gridder='mosaic',field='0,1',  usepointing = True, conjbeams=False, wbawp=True, psterm=False,pblimit=0.1,reffreq='1.5GHz',pbcor=True,mosweight=False,parallel=False)
+
+class MemoryWideFieldAWP(BaseTcleanSetup):
+    """Peak memory benchmarking tests of tclean on widefield using awproject"""
+    def setup(self):
+        self.prepData("refim_mawproject.ms")
+
+    def teardown(self):
+        os.system('rm -rf ' + self.img_subdir)
+        os.system('rm -rf ' + self.img + '*')
+        if (os.path.exists(self.msfile)):
+            os.system('rm -rf ' + self.msfile)
+
+    def peakmem_mfs_awproject_hogbom(self):
+        """tclean: MFS with narrowband AWProjection - test_widefield_aproj_mfs"""
+        ret = tclean(vis=self.msfile, spw='1', field='*', imagename=self.img, imsize=512, cell='10.0arcsec',
+                     phasecenter="J2000 19:59:28.500 +40.44.01.50",niter=30, gridder='awproject', cfcache='',
+                     wbawp=False, conjbeams=True, psterm=False,computepastep=360.0,rotatepastep=360.0,
+                     deconvolver='hogbom', savemodel='modelcolumn', parallel=False)
+
+    def peakmem_mfs_awproject_mtmfs(self):
+        """tclean: MFS with AWProjection and nt=2 stokes I - test_widefield_aproj_mtmfs  """
+        ret = tclean(vis=self.msfile, spw='*', field='*', imagename=self.img, imsize=512, cell='10.0arcsec',
+                     phasecenter="J2000 19:59:28.500 +40.44.01.50",niter=0, gridder='awproject',
+                     cfcache=self.img + '.cfcache', wbawp=True, conjbeams=False, psterm=False,
+                     computepastep=360.0,rotatepastep=360.0, deconvolver='mtmfs', parallel=False)
+
+    def peakmem_cube_awproject_hogbom(self):
+        """tclean: Cube with AW-Projection  and rotation off - test_widefield_aproj_cube"""
+        ret = tclean(vis=self.msfile, field='*', imagename=self.img, imsize=512, cell='10.0arcsec',
+                     phasecenter="J2000 19:59:28.500 +40.44.01.50",specmode='cube', niter=1, gain=1.0,
+                     gridder='awproject', cfcache=self.img + '.cfcache',wbawp=True,conjbeams=False,
+                     psterm=False, computepastep=360.0, rotatepastep=360.0, deconvolver='hogbom',
+                     parallel=False)
+
+class MemorySetWeighting:
     """
     An example benchmark that adapts test_perf_tclean_mem_setweighting.py to asv
     """
@@ -78,9 +191,8 @@ class tclean_memory_suite:
             parallel=False,
         )
 
-    def track_tclean_file_descriptors_cubemode_mosaic_briggs(self):
+    def track_file_descriptors_cubemode_mosaic_briggs(self):
         """Adapted from CAS-8755
-
         https://open-bitbucket.nrao.edu/projects/CASA/repos/casa6/browse/casatests/performance/test_perf_tclean_mem_setweighting.py?at=refs%2Fheads%2FCAS-13026#166-225
 
         Requires user configuration ~/.casa/rc to contain `synthesis.imager.memprofile.enable: 1`
